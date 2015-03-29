f3377a86a57be49fedf312e15013ec273f3ed491
Carolyn-May <PStallion9@gmail.com>
2015-03-28 20:42:25 -0400

    Calendar Boxes
    Start of the calendar boxes and possible weather

---
+++ GoodMorning/app/src/main/java/com/example/nicole/goodmorning/CalendarBoxes.java
@@ -0,0 +1,13 @@
+package com.example.nicole.goodmorning;
+
+/**
+ * Created by Carolyn on 3/28/2015.
+ */
+public class CalendarBoxes {
+    <EditText
+    android:id="@+id/email_address"
+    android:layout_width="fill_parent"
+    android:layout_height="wrap_content"
+    android:hint="@string/email_hint"
+    android:inputType="textEmailAddress" />
+}
